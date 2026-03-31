96397dd3350dcc83838ebedf8b24a42882eec9a6
arsh_katiyar <arsh.katiyar@rapidshyp.com>
2026-03-31 11:42:48 +0530

    second commit

--- consumer/src/main.rs
+++ consumer/src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use rdkafka::consumer::{Consumer, StreamConsumer};
+use rdkafka::ClientConfig;
+use rdkafka::message::Message;
+
+async fn run_consumer(group_id: &str, name: &str,topic_name:&str) {
+    let consumer: StreamConsumer = ClientConfig::new()
+        .set("group.id", group_id)
+        .set("bootstrap.servers", "localhost:9092")
+        .set("auto.offset.reset", "earliest")
+        .create()
+        .expect("Consumer creation failed");
+
+    consumer.subscribe(&[topic_name]).unwrap();
+
+    println!("{} listening...", name);
+
+    loop {
+        match consumer.recv().await {
+            Err(e) => println!("{} error: {}", name, e),
+            Ok(msg) => {
+                if let Some(payload) = msg.payload() {
+                    println!(
+                        "{} received: {}",
+                        name,
+                        String::from_utf8_lossy(payload)
+                    );
+                }
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let c1 = tokio::spawn(run_consumer("group-1", "Consumer-1","test-topic"));
+    let c2 = tokio::spawn(run_consumer("group-2", "Consumer-2","test-topic2"));
+
+    let _ = tokio::join!(c1, c2);
 }
--- producer/src/main.rs
+++ producer/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::time::Duration;
+use rdkafka::ClientConfig;
+use rdkafka::producer::{FutureProducer, FutureRecord};
+
+#[tokio::main]
+async fn main() {
+    let producer: FutureProducer = ClientConfig::new()
+        .set("bootstrap.servers", "localhost:9092")
+        .create()
+        .expect("Producer creation error");
+    let delivery_status = producer.send(FutureRecord::to("test-topic")
+                                            .payload("Hello Kafka from Rust!")
+                                            .key("key1"), Duration::from_secs(0),).await;
+
+    println!("Delivery status: {:?}", delivery_status);
+
+    let delivery_status2 = producer.send(FutureRecord::to("test-topic2")
+                                            .payload("Hello Kafka from Rust2!")
+                                            .key("key2"), Duration::from_secs(0),).await;
+    println!("Delivery status2: {:?}", delivery_status2);
 }
+
